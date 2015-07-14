b4d99abc6c9c84f7c459df4c00e7c9f0f046f481
wuyihao14 <ambulance@mail.dlut.edu.cn>
2015-07-14 13:54:21 +0800

    Correct vertex sets. Add end condition

--- personal_rank.py
+++ personal_rank.py
@@ -3,40 +3,43 @@ __author__ = 'wuyihao'
 import gzip
 import pickle
 import time
+import operator
 
 
 class PersonalRank:
-    def __init__(self):
-        self.rev_map_ = dict()
+    def __init__(self, epsilon):
         self._rank = dict()
         self.map = dict()
         self._r = dict()
+        self._epsilon = epsilon
 
     # TODO randomize, weight by time
 
     def personal_rank(self, d, s):
-        self._rank = dict.fromkeys(self.map.keys() + self.rev_map_.keys(), 0.0)
+        self._rank = dict.fromkeys(self.map.iterkeys(), 0.0)
         self._rank[s] = 1
 
         while True:
             # The iteration is based on the previous vector
             # We need a 2-rolling pair (tmp, rank), to avoid circular dependency in one roll 's inner loop
             if __debug__:
-                print sorted(self._rank.values(), reverse=True)[0:15]
-                time.sleep(1)
-            tmp = dict.fromkeys(self.map.keys() + self.rev_map_.keys(), 0.0)
-            for v in self.rev_map_.keys():
-                for u in self.rev_map_[v]:
+                print self._rank.values()[0:5]
+                time.sleep(0.1)
+            tmp = dict.fromkeys(self.map.iterkeys(), 0.0)
+            for v in self.map.iterkeys():
+                for u in self.map[v]:
                     tmp[v] += 1.0 * d * self._rank[u] / len(self.map[u])
                 # All the other options have the probability: d
                 # The option to start again on 's' is : 1 - d
-                tmp[s] += (1 - d)
+                tmp[s] += 1 - d
+            delta = reduce(operator.add, map(lambda x, y: (x-y)*(x-y), tmp.itervalues(), self._rank.itervalues()))
             self._rank = tmp
+            if delta < self._epsilon:
+                break
 
 class Loader:
-    def __init__(self, map, rev_map):
+    def __init__(self, map):
         self._map = map
-        self._rev_map_ = rev_map
 
     def load_daily_events(self, day):
         db_dir = 'experiments'
@@ -45,17 +48,17 @@ class Loader:
         for d in events:
             u = d['actor']
             v = d['repo']
-            if v not in self._rev_map_:
-                self._rev_map_[v] = set()
-            self._rev_map_[v].add(u)
+            if v not in self._map:
+                self._map[v] = set()
+            self._map[v].add(u)
 
             if u not in self._map:
                 self._map[u] = set()
             self._map[u].add(v)
 
 if __name__ == '__main__':
-    pr = PersonalRank()
-    ld = Loader(pr.map, pr.rev_map_)
+    pr = PersonalRank(1e-10)
+    ld = Loader(pr.map)
     for i in range(8):
         ld.load_daily_events(i)
-    pr.personal_rank(0.8, 'ronhuang')
+    pr.personal_rank(0.2, 'ronhuang')
