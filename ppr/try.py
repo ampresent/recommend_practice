fe2cfac88a4f978c94abab2e03bd879724c64bbd
wuyihao14 <ambulance@mail.dlut.edu.cn>
2015-08-08 20:06:57 +0800

    Interface for SuiteSparse qr demposition

--- ppr/try.py
+++ ppr/try.py
@@ -1,9 +1,22 @@
-import spqr
+import spqr_wrapper
 import os
 import sys
 from scipy.sparse import *
-'''
-A = coo_matrix([[1,2],[0,3]])
-b = 4
-x = spqr.qr_solve(A.data, A.row, A.col, A.nnz, A.shape[0], A.shape[1], b)
-'''
+import numpy as np
+
+A = coo_matrix([[0,4,2],[1,0,3],[4,0,4]], dtype=np.float64)
+b = coo_matrix([[5],[3],[0]], dtype=np.float64)
+
+Z_data, Z_row, Z_col,\
+R_data, R_row, R_col =\
+        spqr_wrapper.qr(\
+        A.data.tolist(),\
+        A.row.tolist(),\
+        A.col.tolist(),\
+        A.shape[0], A.shape[1],\
+        b.data.tolist(), \
+        b.row.tolist())
+print Z_col
+print 'done'
+#print type(E)
+
