dc72778b0d4043dcb04b035b047f2aa20855095a
wuyihao14 <ambulance@mail.dlut.edu.cn>
2015-08-13 10:05:25 +0800

    Use izip instead of zip, dig out csc potential to optimize

--- ppr/ppr.py
+++ ppr/ppr.py
@@ -1,13 +1,15 @@
 # TODO 1. Test Top-k function   2. result decode with p   3. Test With dataset.
 # TODO PO: Potential Optimization   NC: Not clear   m:nonzeroes(numedges)   Convention:'All assesments talk about a sole statement'
 # TODO All the todo, may refer to similar ones, PLEASE / to FIND ALL OCCURRANCE
+# TODO Remove getrow and getcol
+# TODO Reevanluating P's time complexity
 
+import itertools
+import heapq
 import operator
 import numpy
 from scipy import sparse
 from scipy.sparse import linalg
-
-def couple(iterable)
 
 """
 A must be bidirected
@@ -16,28 +18,30 @@ def P(A):
     visited = set()
     p = []
     n = A.shape[0]
+    A_csc = A.tocsc()
     # O(m) PO csr_matrix can reduce
     count = []
-    for x in xrange(n):
-        v = A.getrow(x).nnz
-        count.append((v,-v,x))
+    hash_count = dict()
+    hash_count_orig = dict()
+    for x, group in itertools.groupby(A.row):
+        l = len(list(group))
+        count.append((l,-l,x))
+        hash_count[x] = l
+        hash_count_orig[x] = l
     heapq.heapify(count)
     while count:
     # O(n+m) REMOVE A ROW, one node could be removed more than once
-        v = heapq.heappop()[2]
+        v = heapq.heappop(count)[2]
     # O(1)
-        if v not int visited:
+        if v not in visited:
     # O(n)
             visited.add(v)
     # O(1)
             p.append(v)
-    # o(m)
-
-            colv = A.getcol(v)
-            for ui_left, ui_right in couple(colv.indptr):
-                for u in colv.data[ui_left:ui_right]
-    # o(m)
-                    heapq.heappush((u[0]-1, u[1], u[2]))
+            for ui in xrange(A_csc.indptr[v], A_csc.indptr[v+1]):
+                u = A_csc.indices[ui]
+                hash_count[u] -= 1
+                heapq.heappush(count, (hash_count[u], hash_count_orig[u], u))
     # O(n)
     pt = [0] * n
     # O(n)
@@ -55,23 +59,24 @@ d : seeds set
 """
 def lower_bound(A, d, c):
     # O(n)
-    lb = dict(zip(d.row, d.data * c))
+    lb = dict(itertools.izip(d.row, d.data * c))
     # O(n)
     old_layer = set(d.row)
     visited = set(old_layer)
     # Introduce old_layer and new_layer to enable
     # transition from one WHOLE layer to another
-    while old_layer
+    while old_layer:
         new_layer = set()
     # O(n)
         for u in old_layer:
     # O(m)
             A_relevant = A.getrow(u)
     # O(m)
-            for v, w in zip(A_relevant.indices, A_relevant.data):
+            for v, w in itertools.izip(A_relevant.indices, A_relevant.data):
                 if v not in visited:
     # O(n)
-                    lb.setdefault(v, 0) += (1-c) * w * lb[u]
+                    lb.setdefault(v, 0)
+                    lb[v] += (1-c) * w * lb[u]
                     new_layer.add(v)
         for u in new_layer:
     # O(n)
@@ -95,15 +100,14 @@ def AD(A, pt):
 
 def W(Ad, c):
     # O(m+n)
-    return sparse.eye(A.shape[0]) - (1-c)*Ad
+    return sparse.eye(Ad.shape[0]) - (1-c)*Ad
 
 def QR(w, pt, d):
     w = w.tocoo()
     #print sparse.coo_matrix((d.data, (map(lambda x: pt[x], d.row), [0]*d.nnz))).todense()
     import spqr_wrapper
-    # NC
+    # NC ASSUME it's optical
     # PO if we pass w as csc_matrix , we could avoid 2 transforms
-    # qr: O(???) less than O(n^2) according to the table
     # g: O(|Q|)
     Z_data, Z_row, Z_col,\
     R_data, R_row, R_col =\
@@ -146,9 +150,7 @@ def top_k(lb, g, R, n, K, theta):
     # O(K)
     relevance = [(0, k) for k in xrange(K)]
     heapq.heapify(relevance)
-    # NC 
-    # sparse matrix inverse ?????
-    # should've been less than O(n^3) 'cause sparsity
+    # NC ASSUME it's optical
     R_rev = linalg.inv(R)
     for i in xrange(n):
         # O(n)
@@ -165,7 +167,7 @@ def top_k(lb, g, R, n, K, theta):
                 # Replace v with u, whose relevance is greater
                 # O(n)
                 v = heapq.heappop(relevance)
-                heapq.heappush((relevance_u, u))
+                heapq.heappush(relevance,(relevance_u, u))
                 # O(n)
                 # Refresh theta
                 theta = relevance[0][0]
